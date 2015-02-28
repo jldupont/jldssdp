539d519a4c58a3e9bdc8bb678e2c23ef42ece465
github@jldupont.com <github@jldupont.com>
2015-02-28 15:00:51 -0500

    skeleton

--- src/jldssdp/script_discover.py
+++ src/jldssdp/script_discover.py
@@ -2,10 +2,48 @@
     Created on 2012-01-20
     @author: jldupont
 """
-import logging, os
+import logging
+import socket
 
 
-def run():
+from twisted.internet import epollreactor
+epollreactor.install()
+
+from twisted.internet.protocol import DatagramProtocol
+
+from twisted.internet import reactor
+
+
+class SSDP(DatagramProtocol):
+    """
+    The SSDP protocol
+    """
+    ADDRESS = "239.255.255.250"
+    PORT = 1900
+    
+    def startProtocol(self):
+        """
+        Called after protocol has started listening.
+        """
+        # We don't want this to go very far
+        self.transport.setTTL(1)
+        
+        # Join a specific multicast group:
+        self.transport.joinGroup( self.ADDRESS )
+        
+            
+    def datagramReceived(self, data, (host, port)):
+        
+        logging.info("Data:\n %s" % data)
+
+
+
+def run(interface):
     """
     """
 
+    reactor.listenMulticast(SSDP.PORT 
+                            ,SSDP()
+                            ,listenMultiple=True)
+       
+    reactor.run()
